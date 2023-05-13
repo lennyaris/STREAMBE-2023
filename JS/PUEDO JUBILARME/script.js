4c30dc754a6dc0f28980720cdbaf8c5d56a23008
lenny <lennyaris10@gmail.com>
2023-05-13 12:57:15 -0300

    Ejercicio Calculadora Jubilacion

---
+++ JS/PUEDO JUBILARME/script.js
@@ -0,0 +1,21 @@
+var genero = prompt("Cual es tu genero?")
+
+if (genero === "hombre") {
+    var edadhombre = prompt("Que edad tenes?")
+}   else if(genero==="mujer"){
+    var edadmujer = prompt("Que edad tenes")
+}   else alert("No pusiste ningun genero")
+
+if (edadhombre>="65") {
+    alert("Ya te podes jubilar")
+}   else if(edadmujer>="60"){
+    alert("Ya te podes jubilar")
+}   else alert("No te podes jubilar")
+
+
+
+
+
+
+
+
